8cee2965209edd14944c92ebb9cf9a84d7652b9c
taniamkanna <taniamkanna@hotmail.com>
2025-10-12 23:49:24 +0300

    Add MedicalDocument controller, service, and repository setup

---
+++ backend/src/main/java/com/doctorbookingsystem/doctorbooking/controller/MedicalDocumentController.java
@@ -0,0 +1,9 @@
+package com.doctorbookingsystem.doctorbooking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/medical-documents")
+@RestController
+public class MedicalDocumentController {
+}
